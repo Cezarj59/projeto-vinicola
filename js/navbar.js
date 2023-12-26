71f617da43c27ae93ed382386d9bd5fed9b24429
cezarj59 <cezarj59@yahoo.com>
2023-12-26 16:39:01 -0300

    feat: Adiciona [revenda ao switch display]

--- js/navbar.js
+++ js/navbar.js
@@ -1,30 +1,39 @@
 
-    // Variáveis Html
-    const home = document.getElementById("home-section");
-    const revenda = document.getElementById("revenda-section");
+// Variáveis Html
+const home = document.getElementById("home-section");
+const contato = document.getElementById("contato-section");
+const revenda = document.getElementById("revenda-section");
 
-    // Configuração inicial
-    home.style.display = "block";
-    revenda.style.display = "none";
+// Configuração inicial
+home.style.display = "block";
+contato.style.display = "none";
+revenda.style.display = "none";
 
-    function alternaPaginas(x) {
-        console.log("chamou a função com x =", x);
+function alternaPaginas(x) {
+    console.log("chamou a função com x =", x);
 
-        // Verifique se os elementos estão sendo encontrados
-        console.log("home:", home);
-        console.log("revenda:", revenda);
+    // Verifique se os elementos estão sendo encontrados
+    console.log("home:", home);
+    console.log("revenda:", revenda);
 
-        switch (x) {
-            case 1:
-                home.style.display = "block";
-                revenda.style.display = "none";
-                break;
-            case 8:
-                home.style.display = "none";
-                revenda.style.display = "block";
-                break;
-        }
+    switch (x) {
+        case 1:
+            home.style.display = "block";
+            contato.style.display = "none";
+            revenda.style.display = "none"; 
+            break;
+        case 7:
+            home.style.display = "none";
+            contato.style.display = "block";
+            revenda.style.display = "none";
+            break;
+        case 8:
+            home.style.display = "none";
+            contato.style.display = "none";
+            revenda.style.display = "block";
+            break;
     }
+}
 
 
 
