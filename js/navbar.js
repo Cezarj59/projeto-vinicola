faa79b3af44e159c9e23a8e7b32b56a98ba189eb
cezarj59 <cezarj59@yahoo.com>
2023-12-29 18:16:13 -0300

    feat: Adiciona [galeria.style.display = "none";] Correção de Bug: Inclui section-galeria no switch display

--- js/navbar.js
+++ js/navbar.js
@@ -1,34 +1,45 @@
 
 // Variáveis Html
 const home = document.getElementById("home-section");
+const galeria = document.getElementById("galeria-section");
 const contato = document.getElementById("contato-section");
 const revenda = document.getElementById("revenda-section");
 
 // Configuração inicial
 home.style.display = "block";
+galeria.style.display = "none";
 contato.style.display = "none";
 revenda.style.display = "none";
 
 function alternaPaginas(x) {
-    console.log("chamou a função com x =", x);
+  console.log("chamou a função com x =", x);
 
-     switch (x) {
-        case 0:
-            home.style.display = "block";
-            contato.style.display = "none";
-            revenda.style.display = "none"; 
-            break;
-        case 8:
-            home.style.display = "none";
-            contato.style.display = "block";
-            revenda.style.display = "none";
-            break;
-        case 9:
-            home.style.display = "none";
-            contato.style.display = "none";
-            revenda.style.display = "block";
-            break;
-    }
+  switch (x) {
+    case 0:
+      home.style.display = "block";
+      galeria.style.display = "none";
+      contato.style.display = "none";
+      revenda.style.display = "none";
+      break;
+    case 5:
+      home.style.display = "none";
+      galeria.style.display = "block";
+      contato.style.display = "none";
+      revenda.style.display = "none";
+      break;
+    case 8:
+      home.style.display = "none";
+      galeria.style.display = "none";
+      contato.style.display = "block";
+      revenda.style.display = "none";
+      break;
+    case 9:
+      home.style.display = "none";
+      galeria.style.display = "none";
+      contato.style.display = "none";
+      revenda.style.display = "block";
+      break;
+  }
 }
 
 
@@ -36,45 +47,45 @@ function alternaPaginas(x) {
 /*------------style Navbar---------------*/
 const navStyle = document.getElementsByClassName("destaca-section-atual");
 function active(x) {
-    // Remover a classe "active" de todos os elementos
-    for (let i = 0; i < navStyle.length; i++) {
-      navStyle[i].classList.remove("active");
-    }
-  
-    // Adicionar a classe "active" ao elemento específico
-    if (x >= 0 && x < navStyle.length) {
-      navStyle[x].classList.add("active");
-    }
+  // Remover a classe "active" de todos os elementos
+  for (let i = 0; i < navStyle.length; i++) {
+    navStyle[i].classList.remove("active");
   }
-  
+
+  // Adicionar a classe "active" ao elemento específico
+  if (x >= 0 && x < navStyle.length) {
+    navStyle[x].classList.add("active");
+  }
+}
+
 /*------------offCanvas---------------*/
-  function abrirOffcanvas2() {
-    // Fecha o primeiro offcanvas
-    var offcanvas1 = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
-    offcanvas1.hide();
+function abrirOffcanvas2() {
+  // Fecha o primeiro offcanvas
+  var offcanvas1 = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
+  offcanvas1.hide();
 
-    // Abre o segundo offcanvas
-    var offcanvas2 = new bootstrap.Offcanvas(document.getElementById('offcanvas2'));
-    offcanvas2.show();
-  }
+  // Abre o segundo offcanvas
+  var offcanvas2 = new bootstrap.Offcanvas(document.getElementById('offcanvas2'));
+  offcanvas2.show();
+}
 
-  function fecharOffcanvas2() {
+function fecharOffcanvas2() {
 
-    // Fecha o segundo offcanvas
-    var offcanvas2 = new bootstrap.Offcanvas(document.getElementById('offcanvas2'));
-    offcanvas2.hide();
+  // Fecha o segundo offcanvas
+  var offcanvas2 = new bootstrap.Offcanvas(document.getElementById('offcanvas2'));
+  offcanvas2.hide();
 
-    // Abre o primeiro offcanvas
-    var offcanvas1 = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
-    offcanvas1.show();
-
-
-  }
-
-  function fecharOffcanvas() {
-    // Fecha o primeiro offcanvas
-    var offcanvas1 = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
-    offcanvas1.hide();
+  // Abre o primeiro offcanvas
+  var offcanvas1 = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
+  offcanvas1.show();
 
 
-  }
+}
+
+function fecharOffcanvas() {
+  // Fecha o primeiro offcanvas
+  var offcanvas1 = new bootstrap.Offcanvas(document.getElementById('offcanvasNavbar'));
+  offcanvas1.hide();
+
+
+}
