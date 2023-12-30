148fa8d5581eaffa3290386ee88548a2499acdda
cezarj59 <cezarj59@yahoo.com>
2023-12-30 12:34:47 -0300

    feat: Adiciona [window.onscroll ] Correção de Bug: Faz o botão voltar ao topo aparecer só quando necessario.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+window.onscroll = function() {
+  var botaoVoltarAoTopo = document.getElementById('btnVoltarAoTopo');
+  
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    botaoVoltarAoTopo.classList.remove('d-none');
+
+  } else {
+    botaoVoltarAoTopo.classList.add('d-none');
+  }
+};
+
 function voltarAoTopo() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
