db3c12626c919b570fb07f037ba3b9051da47d7e
cezarj59 <cezarj59@yahoo.com>
2024-01-14 21:26:54 -0300

    feat: Adiciona [carrinhoCompras] Correção de Bug:  carrinhoCompras.style.display = "none";

--- js/navbar.js
+++ js/navbar.js
@@ -2,13 +2,16 @@
 // Variáveis Html
 const home = document.getElementById("home-section");
 const loja = document.getElementById("loja-section");
+const  carrinhoCompras = document.getElementById("carrinho-section");
 const galeria = document.getElementById("galeria-section");
 const contato = document.getElementById("contato-section");
 const revenda = document.getElementById("revenda-section");
 
+
 // Configuração inicial
 home.style.display = "block";
 loja.style.display = "none";
+ carrinhoCompras.style.display = "none";
 galeria.style.display = "none";
 contato.style.display = "none";
 revenda.style.display = "none";
@@ -18,42 +21,56 @@ function alternaPaginas(x) {
 
   switch (x) {
     case 0:
-      document.title =  "Home";
+      document.title = "Home";
       home.style.display = "block";
       loja.style.display = "none";
+       carrinhoCompras.style.display = "none";
       galeria.style.display = "none";
       contato.style.display = "none";
       revenda.style.display = "none";
       break;
 
     case 1:
-      document.title =  "Loja";
+      document.title = "Loja";
       home.style.display = "none";
       loja.style.display = "block";
+       carrinhoCompras.style.display = "none";
+      galeria.style.display = "none";
+      contato.style.display = "none";
+      revenda.style.display = "none";
+      break;
+    case 2:
+      document.title = " Carrinho";
+      home.style.display = "none";
+      loja.style.display = "none";
+       carrinhoCompras.style.display = "block";
       galeria.style.display = "none";
       contato.style.display = "none";
       revenda.style.display = "none";
       break;
     case 5:
-      document.title =  "Galeria";
+      document.title = "Galeria";
       home.style.display = "none";
       loja.style.display = "none";
+       carrinhoCompras.style.display = "none";
       galeria.style.display = "block";
       contato.style.display = "none";
       revenda.style.display = "none";
       break;
     case 8:
-      document.title =  "Contato";
+      document.title = "Contato";
       home.style.display = "none";
       loja.style.display = "none";
+       carrinhoCompras.style.display = "none";
       galeria.style.display = "none";
       contato.style.display = "block";
       revenda.style.display = "none";
       break;
     case 9:
-      document.title =  "Revenda";
+      document.title = "Revenda";
       home.style.display = "none";
       loja.style.display = "none";
+       carrinhoCompras.style.display = "none";
       galeria.style.display = "none";
       contato.style.display = "none";
       revenda.style.display = "block";
